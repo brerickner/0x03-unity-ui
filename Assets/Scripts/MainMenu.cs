fdd8937af82759b114696fa769a0e4d9cb046a4c
Bre <49533377+brerickner@users.noreply.github.com>
2021-08-09 17:58:39 -0500

    saving progress after final task

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,23 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    
     /// <summary>
     /// Loads the maze scene when the Play button is pressed
     /// </summary>
     public void PlayMaze(){
+        if (colorblindMode.isOn){
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     
